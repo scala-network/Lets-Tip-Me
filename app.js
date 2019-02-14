64d8c92015bf5697317a064512c9345ac7a2ff85
Ox HaK <github@oxhak.fr>
2019-02-14 13:26:31 +0100

    Material design icons remove

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ var app = express();
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
 app.get('/about', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
 app.use(express.static('public'));
-
 app.listen(3000);
