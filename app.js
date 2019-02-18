d98ddfc6fb29eb09f021c90dcd80d977f03e6a46
Ox HaK <github@oxhak.fr>
2019-02-18 11:06:53 +0100

    Login update

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -11,5 +17,11 @@ app.get('/login', function(req, res) {
 app.get('/about', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/login', (req, res) => {
+  var email = req.body.email, password = req.body.password;
+  res.send("You posted email: "+ email +" & password: "+ password +" to the login page!\n");
+});
+
 app.use(express.static('public'));
 app.listen(3000);
--- public/js/jquery-stellite-funding.js
+++ public/js/jquery-stellite-funding.js
@@ -1,8 +1,18 @@
 $( document ).ready(function() {
+// body load
     if ( $(location).attr('pathname') == "/about" ) {
     $( "#body-load" ).load( "/about.html" );
     }
     else if ( $(location).attr('pathname') == "/login" ) {
     $( "#body-load" ).load( "/login.html" );
     }
+// login
+var email,password;
+$( document ).on( 'click', '#Login', function () {
+  email=$("#email").val();
+  password=$("#password").val();
+  $.post("/login",{email: email,password: password}, function(data){
+        alert(data);
+  });
+     });
 });
