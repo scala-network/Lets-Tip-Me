b74db0922ccebd4b650add7864b2bd15d21d5bdc
Ox HaK <github@oxhak.fr>
2019-03-13 09:54:17 +0100

    Added copy address to clipboard

--- public/js/jquery-stellite-funding.js
+++ public/js/jquery-stellite-funding.js
@@ -15,6 +15,13 @@ $( document ).ready(function() {
   };
   var activation_success = getUrlParameter('activation');
 
+  function copyToClipboard(element) {
+   var $temp = $("<input>");
+   $("body").append($temp);
+   $temp.val($(element).html()).select();
+   document.execCommand("copy");
+   $temp.remove();
+  }
 
   //body interactions
   $( document ).on( 'mouseover', '.goal_link', function () {
@@ -27,6 +34,41 @@ $( document ).ready(function() {
   $( document ).on( 'click', '.goal_link', function () {
     $(location).attr('href', $(this).attr('goallink'));
   });
+
+  ///copy address to donate to clipboard
+  $( document ).on( 'mouseover', '.goal_donate_copy', function () {
+    $(this).css('cursor','pointer');
+    $(this).css('background-color','#151b29');
+  });
+  $( document ).on( 'mouseout', '.goal_donate_copy', function () {
+    $(this).css('background-color','#11141b');
+  });
+  $( document ).on( 'click', '.goal_donate_copy', function () {
+    copyToClipboard('#addr_to_copy');
+    $('#confirm_addr_to_copy_user').removeClass("text-success");
+    $('#confirm_addr_to_copy_user').html("<i class=\"far fa-copy\"></i> Copy to donate as " + $('#confirm_addr_to_copy_user').attr('username'));
+
+    $('#confirm_addr_to_copy').addClass("text-success");
+    $('#confirm_addr_to_copy').html("<i class=\"fas fa-clipboard-check\"></i> Copied to clipboard to donate!");
+  });
+
+  ///copy address to donate as user to clipboard
+  $( document ).on( 'mouseover', '.goal_donate_copy_user', function () {
+    $(this).css('cursor','pointer');
+    $(this).css('background-color','#151b29');
+  });
+  $( document ).on( 'mouseout', '.goal_donate_copy_user', function () {
+    $(this).css('background-color','#11141b');
+  });
+  $( document ).on( 'click', '.goal_donate_copy_user', function () {
+    copyToClipboard('#addr_to_copy_user');
+    $('#confirm_addr_to_copy').removeClass("text-success");
+    $('#confirm_addr_to_copy').html("<i class=\"far fa-copy\"></i> Copy to donate");
+
+    $('#confirm_addr_to_copy_user').addClass("text-success");
+    $('#confirm_addr_to_copy_user').html("<i class=\"fas fa-clipboard-check\"></i> Copied to clipboard to donate as " + $('#confirm_addr_to_copy_user').attr('username')+"!");
+  });
+
   // body load
   if ( $(location).attr('pathname') == "/about" ) {
     $( "#body-load" ).load( "/about.html" );
@@ -103,8 +145,9 @@ $( document ).ready(function() {
           $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite text-left\"><span class=\"text-white\"><p>On October 28, 2017, BitConnect held its first annual ceremony in Pattaya, Thailand. During the event, Carlos Matos from New York gave an enthusiastic presentation and testimonial about the website which led to him becoming an internet meme.</p></li>");
 
           //goal donations address
-          $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite \"><span class=\"text-white\"><small><b>Se2K2QkvTcvCwd8cjsjK5YDxkrizwTSAFga8X8zBYuXdUiV46fHiQ5E5A43krzyxMvBtoM38HhcJM9QDa6sy6WAv1yd2gUem8</b></small><br><small><i class=\"far fa-copy\"></i> Copy to donate as oxhak</small></li>");
-          $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite \"><span class=\"text-white\"><small><b>Se3giBG4UnbV3PaTpT4ezJjdnDrAeDLHaeZS3Kqe6KPcZD6BfL8B9NLaXcyRMtwh332a97WD92enaDvPNigb4CwB1r5uchuHD</b></small><br><small><i class=\"far fa-copy\"></i> Copy to donate</small></li>");
+          $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite goal_donate_copy_user \"><span class=\"text-white\"><small><b id=\"addr_to_copy_user\">Se2K2QkvTcvCwd8cjsjK5YDxkrizwTSAFga8X8zBYuXdUiV46fHiQ5E5A43krzyxMvBtoM38HhcJM9QDa6sy6WAv1yd2gUem8</b></small><br><small id=\"confirm_addr_to_copy_user\" username=\"oxhak\"><i class=\"far fa-copy\"></i> Copy to donate as oxhak</small></li>");
+
+          $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite goal_donate_copy\"><span class=\"text-white\"><small><b id=\"addr_to_copy\">Se3giBG4UnbV3PaTpT4ezJjdnDrAeDLHaeZS3Kqe6KPcZD6BfL8B9NLaXcyRMtwh332a97WD92enaDvPNigb4CwB1r5uchuHD</b></small><br><small id=\"confirm_addr_to_copy\"><i class=\"far fa-copy\"></i> Copy to donate</small></li>");
 
           //goal by
           $("#funding_goal_progress").append("<li class=\"list-group-item justify-content-between list-group-item-stellite stellite-lighter-background\"><span class=\"text-white\"><small>Goal added by oxhak · March 11, 2019</small></li>");
@@ -116,7 +159,7 @@ $( document ).ready(function() {
           $("#funding_goal").append("<div class=\"col-md-12\"><h4 class=\"d-flex justify-content-between align-items-center mb-3\"><h4 class=\"text-center\"><br>Transactions History</h4></h4><hr class=\"mb-4\"><ul id=\"funding_goal_transactions\" class=\"list-group\">");
 
           //transactions
-          $("#funding_goal_transactions").append("<li class=\"list-group-item justify-content-between list-group-item-stellite\"><span class=\"text-white\"><small><span class=\"text-success\">+12165.70 XTL</span> · TX <a href=\"https://explorer.stellite.cash/tx/1d23778925306ce10f3a308af6bdd061c01bf2576259f7ee4791c2c786856d53\" target=\"_blank\">1d23778925306ce10f3a308af6bdd061c01bf2576259f7ee4791c2c786856d53</a> · 2019-03-11 10:29:45</small></li>");
+          $("#funding_goal_transactions").append("<li class=\"list-group-item justify-content-between list-group-item-stellite \"><span class=\"text-white\"><small><span class=\"text-success\">+12165.70 XTL</span> · TX <a href=\"https://explorer.stellite.cash/tx/1d23778925306ce10f3a308af6bdd061c01bf2576259f7ee4791c2c786856d53\" target=\"_blank\">1d23778925306ce10f3a308af6bdd061c01bf2576259f7ee4791c2c786856d53</a> · 2019-03-11 10:29:45</small></li>");
           $("#funding_goal_transactions").append("<li class=\"list-group-item justify-content-between list-group-item-stellite\"><span class=\"text-white\"><small><span class=\"text-success\">+12169.30 XTL</span> · TX <a href=\"https://explorer.stellite.cash/tx/1d357b1baeb22ecf8be4554f84b794a2309c3473cbc40a80628ff23890e227fa\" target=\"_blank\">1d357b1baeb22ecf8be4554f84b794a2309c3473cbc40a80628ff23890e227fa</a> · 2019-03-11 10:20:19</small></li>");
 
           //transactions history end
