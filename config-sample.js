b984f80366d0a949acb4a8e1483e5fdbba15f49d
Ox HaK <github@oxhak.fr>
2019-04-13 08:42:31 +0200

    Config file update

--- config-sample.js
+++ config-sample.js
@@ -1,9 +1,14 @@
 var config = {};
 
-config.env = 'dev';
+config.env = "dev";
 config.https_cert = '';
 config.https_key = '';
-config.rpc_wallet_address = '127.0.0.1';
-config.rpc_wallet_port = '18082';
+config.rpc_wallet_address = "127.0.0.1";
+config.rpc_wallet_port = "18082";
+
+//In production sendmail_devPort="null" sendmail_devHost="null" smtpPort="25"
+config.sendmail_devPort = "25";
+config.sendmail_devHost = "devsmtphost";
+config.smtpPort = "";
 
 module.exports = config;
