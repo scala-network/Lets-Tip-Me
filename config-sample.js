9e64493b5e6528bfd6130cadb10310c0072e4df2
Ox HaK <github@oxhak.fr>
2019-04-13 10:45:39 +0200

    Config sample update

--- config-sample.js
+++ config-sample.js
@@ -1,12 +1,20 @@
 var config = {};
 
+////Dev env
 config.env = 'dev';
 config.https_cert = '';
 config.https_key = '';
+
+////Production env
+// config.env = 'production';
+// config.https_cert = 'PATH/TO/YOUR/SSL/CERTFICIATE';
+// config.https_key = 'PATH/TO/YOUR/SSL/PRIVATE_KEY';
+
+////RPC Wallet
 config.rpc_wallet_address = '127.0.0.1';
 config.rpc_wallet_port = '18082';
 
-//In production sendmail_devPort='' sendmail_devHost='' smtpPort='25'
+////In production sendmail_devPort='' sendmail_devHost='' smtpPort='25'
 config.sendmail_devPort = '25';
 config.sendmail_devHost = 'localhost';
 config.smtpPort = '';
